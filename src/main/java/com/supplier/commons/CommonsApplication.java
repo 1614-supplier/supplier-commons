82a4301ef12e0c080771d7d3b134d89c01aac9ef
zhenhai.zheng <zhenhai.zheng@foxmail.com>
2018-11-29 14:33:43 +0800

    修改路径

--- src/main/java/com/supplier/commons/CommonsApplication.java
+++ src/main/java/com/supplier/commons/CommonsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SupplierUserApplication {
+public class CommonsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SupplierUserApplication.class, args);
+		SpringApplication.run(CommonsApplication.class, args);
 	}
 }
